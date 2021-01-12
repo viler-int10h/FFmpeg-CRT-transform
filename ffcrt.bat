a7de400ab208bd852e107708e45965263b6ca4a1
VileR <abovetheruins@gmail.com>
2021-01-12 23:44:57 +0200

    Single script to handle both images and videos

--- ffcrt.bat
+++ ffcrt.bat
@@ -1,18 +1,20 @@
+:: FFmpeg CRT transform script / VileR 2021
 :: parameter 1 = config file
-:: parameter 2 = input video
-:: parameter 3 = output video
+:: parameter 2 = input video/image
+:: parameter 3 = output video/image
 
 ::+++++++++++++++++++++++++::
 :: Check cmdline arguments ::
 ::+++++++++++++++++++++++++::
 
 @echo off & setlocal EnableDelayedExpansion
-::setlocal EnableDelayedExpansion
 
 if [%1] neq [] if [%2] neq [] goto :ARGS_OK
-	echo. & echo USAGE:    %~n0 configFile inputVideo [outputVideo]
-	echo. & echo   If outputVideo is omitted, the input filename will be appended with
-	echo   "_OUT" for the output file. & exit /b
+  echo. & echo FFmpeg CRT transform script / VileR 2021
+	echo. & echo USAGE:  %~n0 ^<config_file^> ^<input_file^> [output_file] & echo.
+	echo    input_file must be a valid image or video.  If output_file is omitted, the
+	echo    output will be named "(input_file)_OUT.(input_ext)"
+	exit /b
 :ARGS_OK
 	if [%3] neq [] (
 		set OUTFILE=%~f3
@@ -36,14 +38,31 @@ if [%1] neq [] if [%2] neq [] goto :ARGS_OK
 :: Find input dimensions ::
 ::+++++++++++++++++++++++::
 
-set IX= & SET IY= & for /f %%i IN ('ffprobe -hide_banner -show_entries stream^=width^,height %2 ^| find "="') do (
+set IX= & SET IY= & for /f %%i IN ('ffprobe -hide_banner -loglevel quiet -show_entries stream^=width^,height %2 ^| find "="') do (
 	set x=%%i
 	if "!x:~0,6!"=="width=" SET IX=!x:~6!
 	if "!x:~0,7!"=="height=" SET IY=!x:~7!
 )
 if "%IX%" neq " " if "%IY%" neq " " goto :ALL_OK
-echo. & echo Couldn't get valid width/height for input video "%2"^^^! & exit /b
+echo. & echo Couldn't get valid width/height for input file "%2"^^^! & exit /b
 :ALL_OK
+
+
+::+++++++++++++++++++++++++++++++++++++++++++::
+:: Count frames - is it an image or a video? ::
+::+++++++++++++++++++++++++++++++++++++++++++::
+
+for /f "usebackq tokens=2 delims== " %%i in (`ffmpeg -i %2 -map 0:v:0 -c copy -f null - 2^>^&1 ^| find "frame="`) do SET NUM_FRAMES=%%i
+if errorlevel 1 (echo Couldn't get frame count for input file "%2"^^^! & exit /b)
+if "%NUM_FRAMES%" == "1" (
+	set TMP_EXT=png
+	set TMP_OUTPARAMS= 
+	set FIN_OUTPARAMS= 
+) else (
+	set TMP_EXT=avi
+	set TMP_OUTPARAMS=-c:a copy -c:v libx264rgb -crf 0
+	set FIN_OUTPARAMS=-c:a copy -c:v libx264rgb -crf 8
+)
 
 
 ::++++++++++::
@@ -130,7 +149,7 @@ if /i "%SCANLINES_ON%"=="yes" (
 :: Tile shadowmask, add curvature ::
 ::********************************::
 
-ffmpeg -hide_banner -y -loop 1 -framerate 1 -t 80^
+ffmpeg -hide_banner -y -loop 1 -framerate 1 -t 100^
 	-i _%OVL_TYPE%.png^
 	-filter_complex ^"^
 		lutrgb='r=gammaval(2.2):g=gammaval(2.2):b=gammaval(2.2)',^
@@ -149,22 +168,22 @@ ffmpeg -hide_banner -y -loop 1 -framerate 1 -t 160^
 if errorlevel 1 exit /b
 
 
-::++++++++++++++++::
-:: Phosphor decay ::
-::++++++++++++++++::
+::+++++++++++++++++++++++++++++::
+:: Phosphor decay - video only ::
+::+++++++++++++++++++++++++++++::
 
-if "%P_DECAY_FACTOR%"=="0" (set SCALESRC=%2) else (
-	ffmpeg -hide_banner -y^
-	-i %2 ^
-	-filter_complex ^"^
-		[0] split [orig][2lag]; ^
-		[2lag] lagfun=%P_DECAY_FACTOR% [lag]; ^
-		[orig][lag] blend=all_mode='lighten':all_opacity=%P_DECAY_ALPHA%^" ^
-	-c:v libx264rgb -crf 0 TMPstep00.avi
-	if errorlevel 1 exit /b
-	SET SCALESRC=TMPstep00.avi
+SET SCALESRC=%2
+if "%IS_VIDEO%" == "1" if %P_DECAY_FACTOR% gtr 0 (
+		ffmpeg -hide_banner -y^
+		-i %2 ^
+		-filter_complex ^"^
+			[0] split [orig][2lag]; ^
+			[2lag] lagfun=%P_DECAY_FACTOR% [lag]; ^
+			[orig][lag] blend=all_mode='lighten':all_opacity=%P_DECAY_ALPHA%^" ^
+		-c:v libx264rgb -crf 0 TMPstep00.%TMP_EXT%
+		if errorlevel 1 exit /b
+		SET SCALESRC=TMPstep00.%TMP_EXT%
 )
-
 
 ::++++++++++++++++++++++++++++++++++++++++++++++++++::
 :: Scale nearest neighbor, apply gamma & pixel blur ::
@@ -176,10 +195,9 @@ ffmpeg -hide_banner -y^
 		scale=%PX%:%PY%:flags=neighbor+%SWSFLAGS%,^
 		lutrgb='r=gammaval(2.2):g=gammaval(2.2):b=gammaval(2.2)',^
 		gblur=sigma=%H_PX_BLUR%/100*%SX%:sigmaV=%VSIGMA%:steps=3^"^
-	-c:a copy -c:v libx264rgb -crf 0 TMPstep01.avi
+	%TMP_OUTPARAMS% TMPstep01.%TMP_EXT%
 
 if errorlevel 1 exit /b
-
 
 ::++++++++++++++++++++++++++++++++++++++++++++::
 :: Add halation, revert gamma + add curvature ::
@@ -187,21 +205,21 @@ if errorlevel 1 exit /b
 
 if /i "%HALATION_ON%"=="yes" (
 	ffmpeg -hide_banner -y^
-	-i TMPstep01.avi^
+	-i TMPstep01.%TMP_EXT%^
 	-filter_complex ^"^
 		[0]split[a][b],^
 		[a]gblur=sigma=%HALATION_RADIUS%:steps=3[h],^
 		[b][h]blend=all_mode='lighten':all_opacity=%HALATION_ALPHA%,^
 		lutrgb='r=gammaval^(0.454545^):g=gammaval^(0.454545^):b=gammaval^(0.454545^)'^
 		%LENSC%^"^
-	-c:a copy -c:v libx264rgb -crf 0 TMPstep02.avi
+	%TMP_OUTPARAMS% TMPstep02.%TMP_EXT%
 ) else (
 	ffmpeg -hide_banner -y^
-	-i TMPstep01.avi^
+	-i TMPstep01.%TMP_EXT%^
 	-vf ^"^
 		lutrgb='r=gammaval^(0.454545^):g=gammaval^(0.454545^):b=gammaval^(0.454545^)'^
 		%LENSC%^"^
-	-c:a copy -c:v libx264rgb -crf 0 TMPstep02.avi
+	%TMP_OUTPARAMS% TMPstep02.%TMP_EXT%
 )
 if errorlevel 1 exit /b
 
@@ -215,43 +233,43 @@ if /i "%SCANLINES_ON%"=="yes" (
 	if /i "%BLOOM_ON%"=="yes" (
 
 		ffmpeg -hide_banner -y^
-		-i TMPscanlines.png -i TMPstep02.avi^
+		-i TMPscanlines.png -i TMPstep02.%TMP_EXT%^
 		-filter_complex ^"^
 			[1]lutrgb='r=gammaval^(2.2^):g=gammaval^(2.2^):b=gammaval^(2.2^)', hue=s=0, lutrgb='r=gammaval^(0.454545^):g=gammaval^(0.454545^):b=gammaval^(0.454545^)'[g],^
 			[g][0]blend=all_expr='if^(gte^(A,128^), ^(B+^(255-B^)*%BLOOM_POWER%*^(A-128^)/128^), B^)',^
 			setsar=sar=1/1^"^
-		-an -c:v libx264rgb -crf 0 TMPbloom.avi
+		%TMP_OUTPARAMS% TMPbloom.%TMP_EXT%
 
 		ffmpeg -hide_banner -y^
-		-i TMPstep02.avi -i TMPbloom.avi -i TMPshadowmask.png -i TMPcorners.png^
+		-i TMPstep02.%TMP_EXT% -i TMPbloom.%TMP_EXT% -i TMPshadowmask.png -i TMPcorners.png^
 		-filter_complex ^"^
 			[0][1]blend=all_mode='multiply':all_opacity=%SL_ALPHA%[a],^
 			[a][2]blend=all_mode='multiply':all_opacity=%OVL_ALPHA%[b],^
 			[b][3]overlay=0:0:format=rgb,^
 			lutrgb='r=clip^(val*%OVL_BRIGHTFIX%,0,255^):g=clip^(val*%OVL_BRIGHTFIX%,0,255^):b=clip^(val*%OVL_BRIGHTFIX%,0,255^)'^"^
-		-c:a copy -c:v libx264rgb -crf 0 TMPstep03.avi
+		%TMP_OUTPARAMS% TMPstep03.%TMP_EXT%
 
 	) else (
 
 		ffmpeg -hide_banner -y^
-		-i TMPstep02.avi -i TMPscanlines.png -i TMPshadowmask.png -i TMPcorners.png^
+		-i TMPstep02.%TMP_EXT% -i TMPscanlines.png -i TMPshadowmask.png -i TMPcorners.png^
 		-filter_complex ^"^
 			[0][1]blend=all_mode='multiply':all_opacity=%SL_ALPHA%[a],^
 			[a][2]blend=all_mode='multiply':all_opacity=%OVL_ALPHA%[b],^
 			[b][3]overlay=0:0:format=rgb,^
 			lutrgb='r=clip^(val*%OVL_BRIGHTFIX%,0,255^):g=clip^(val*%OVL_BRIGHTFIX%,0,255^):b=clip^(val*%OVL_BRIGHTFIX%,0,255^)'^"^
-		-c:a copy -c:v libx264rgb -crf 0 TMPstep03.avi
+		%TMP_OUTPARAMS% TMPstep03.%TMP_EXT%
 	)
 
 ) else (
 
 	ffmpeg -hide_banner -y^
-	-i TMPstep02.avi -i TMPshadowmask.png -i TMPcorners.png^
+	-i TMPstep02.%TMP_EXT% -i TMPshadowmask.png -i TMPcorners.png^
 	-filter_complex ^"^
 		[0][1]blend=all_mode='multiply':all_opacity=%OVL_ALPHA%[b],^
 		[b][2]overlay=0:0:format=rgb,^
 		lutrgb='r=clip^(val*%OVL_BRIGHTFIX%,0,255^):g=clip^(val*%OVL_BRIGHTFIX%,0,255^):b=clip^(val*%OVL_BRIGHTFIX%,0,255^)'^"^
-	-c:a copy -c:v libx264rgb -crf 0 TMPstep03.avi
+	%TMP_OUTPARAMS% TMPstep03.%TMP_EXT%
 )
 
 if errorlevel 1 exit /b
@@ -271,7 +289,7 @@ if errorlevel 1 exit /b
 for /f "tokens=*" %%a IN (TMPcrop) do set CROPTEMP=%%a
 for %%a IN (%CROPTEMP%) do set CROP_STR=%%a
 
-ffmpeg -hide_banner -y -i TMPstep03.avi -vf ^"^
+ffmpeg -hide_banner -y -i TMPstep03.%TMP_EXT% -vf ^"^
 	crop=%CROP_STR%,^
 	lutrgb='r=gammaval(2.2):g=gammaval(2.2):b=gammaval(2.2)',^
 	scale=w=%OX%-%OMARGIN%*2:h=%OY%-%OMARGIN%*2:force_original_aspect_ratio=decrease:flags=%OFILTER%+%SWSFLAGS%,^
@@ -279,8 +297,9 @@ ffmpeg -hide_banner -y -i TMPstep03.avi -vf ^"^
 	setsar=sar=1/1,^
 	%VIGNETTE_STR%^
 	pad=%OX%:%OY%:-1:-1:black^" ^
--c:a copy -c:v libx264rgb -crf 8 "%OUTFILE%"
+%FIN_OUTPARAMS% "%OUTFILE%"
 if errorlevel 1 exit /b
+
 
 ::++++++++++::
 :: Clean up ::
